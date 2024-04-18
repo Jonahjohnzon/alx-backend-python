32c4297968455ac15c9a4e5cedebec3c4078ce2b
jonah ominyi <jonahjohnzon@gmail.com>
2024-04-18 11:43:44 +0000

    python

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,8 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''
-    Converts a key 
-    '''
-
+    '''Converts a key '''
     return (k, float(v**2))
