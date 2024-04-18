ab02e69e9ab4e56e69beea0e146d2b077fe22408
jonah ominyi <jonahjohnzon@gmail.com>
2024-04-18 11:26:06 +0000

    python

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+""" list of floats """
+from typing import Callable, Iterator, Union, Optional, List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    returns their sum as a float.
+    """
+
+    return sum(input_list)
