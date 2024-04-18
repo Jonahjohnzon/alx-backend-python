503b6801e4a88736e5efcf3415efa823de641df1
jonah ominyi <jonahjohnzon@gmail.com>
2024-04-18 11:32:35 +0000

    python

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-"""String and int/float to tuple"""
-from typing import Callable, Iterator, Union, Optional, List, Tuple
+'''String and int/float to tuple'''
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """
-    returns a tuple.
-    """
-
-    return (k, v**2)
+    '''Converts a key 
+    '''
+    return (k, float(v**2))
